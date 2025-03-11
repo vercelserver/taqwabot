8e865dffaf58cf5f1c85f98abc6adf6c69462fc7
Nabijon Ibodullaev <nabijonibodullaev@gmail.com>
2025-03-11 11:57:59 +0500

    hello

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const { schedulePrayerReminders } = require("./src/services/prey-time-remainder.service");
 const userCommands = require('./src/commands/user.command');
+const express = require("express");
 
-// run database 
-require('./src/database/db');
-require('./src/commands/bot.command')
+const app = express();
+
+async function start() {
+    try {
+        require('./src/database/db');
+        require('./src/commands/bot.command')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
 
 // telegram bot commands
 userCommands();
 schedulePrayerReminders();
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
