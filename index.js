70364ed6f40ff9200aa427be7eb689adde12d4aa
Nabijon Ibodullaev <nabijonibodullaev@gmail.com>
2025-03-11 13:49:36 +0500

    change

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const { connectDB } = require("./src/database/db");
 const app = express();
 
 connectDB().then(async () => {
-    await userCommands();
     await schedulePrayerReminders();
-    require('./src/commands/bot.command')
-    app.listen(3000, () => console.log("Server is running on port 3000"));
 }).catch(err => console.error(err)).finally(() => {
     console.log("Server is running on port 3000");
 });
+
+require('./src/commands/bot.command')
+userCommands();
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
